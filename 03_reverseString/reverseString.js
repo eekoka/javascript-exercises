04118380fc45511ab20bf55ba959607fc0e85fb4
ekoka <66888386+eekoka@users.noreply.github.com>
2023-03-11 00:14:45 +0100

    js exercise 03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(text) {
+  let result = "";
 
+  for (let i = 1; i <= text.length; i++) {
+    result += text.substr(-i, 1);
+  }
+  return result;
 };
 
 // Do not edit below this line
