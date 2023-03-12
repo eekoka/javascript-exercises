68e8d660b3d6be0b2ac7ea6b8411601e10a66070
ekoka <66888386+eekoka@users.noreply.github.com>
2023-03-12 02:14:05 +0100

    js exercise 04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(...allArgs) {
+   let baseArray = allArgs[0];
+   let removeValues = allArgs;
+   removeValues.shift();
 
+   for (const value of removeValues) {
+     if (baseArray.indexOf(value) >= 0) {
+        baseArray.splice(baseArray.indexOf(value),1);
+     }
+   }
+   return baseArray;
 };
 
 // Do not edit below this line
