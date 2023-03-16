7e942e6bfafede4f92c5b52640b36b56b980a6c4
ekoka <66888386+eekoka@users.noreply.github.com>
2023-03-16 14:06:54 +0100

    js exercise 09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+  //using regular expression remove non-letters
+  //changing to lower case and converting to array
+  //comparing with reverse of the string
+  return text.replace(/\W/g, "").toLowerCase() ===
+         text.replace(/\W/g, "").toLowerCase().split("").reverse().join("");
 };
 
 // Do not edit below this line
