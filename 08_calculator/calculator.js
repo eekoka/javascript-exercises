fcfec24bf6a5d3414003b5d5cf5d29dd375ad17d
ekoka <66888386+eekoka@users.noreply.github.com>
2023-03-16 02:29:39 +0100

    js exercise 08 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(input) {
+  if (input.length === 0) return 0;
+
+  let ans = 0;
+  input.forEach(myFxn);
+  function myFxn(value) {
+    ans += value;
+  }
+  return ans;
 };
 
-const multiply = function() {
-
+const multiply = function(input) {
+  let ans = 1;
+  input.forEach(myFxn);
+  function myFxn(value) {
+    ans *= value;
+  }
+  return ans;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let ans;
+  //zero factorial is 1
+	if (a === 0) {
+    ans = 1;
+  } else if (Number.isInteger(a) === true && a > 0) {
+    ans = 1;
+    for (let i=1; i <= a; i++) {
+      ans *= i;
+    }
+  } else {
+    return "ERROR";
+  }
+  return ans;
 };
 
 // Do not edit below this line
