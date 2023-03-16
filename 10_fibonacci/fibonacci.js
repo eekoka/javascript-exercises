524e2c722d9e9e7353ce6eb6c108d9e88b4091c3
ekoka <66888386+eekoka@users.noreply.github.com>
2023-03-16 16:05:28 +0100

    js exercise 10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  //reject if number is less than 0 or not an integer
+  if (n < 0) return "OOPS";
+  //returning the nth fibonacci number
+  return (
+    Math.round(
+        (Math.pow(1 + Math.sqrt(5),n) - Math.pow(1 - Math.sqrt(5),n)) / (Math.pow(2,n) * Math.sqrt(5))
+    )
+  )
 
 };
 
