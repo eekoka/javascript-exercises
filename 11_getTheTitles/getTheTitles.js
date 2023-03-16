0740b51b721578f4179c937988d3882ef504adee
ekoka <66888386+eekoka@users.noreply.github.com>
2023-03-16 21:39:26 +0100

    js exercise 11 done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(arrObj) {
+  /*let bookTitles = [];
+  arrObj.forEach(extractTitle);
+  function extractTitle (item) {
+    bookTitles.push(item.title);
+  }
+  return bookTitles;*/
 
+  //shorter alternative solution
+  return arrObj.map((bookz) => bookz.title)
 };
 
 // Do not edit below this line
