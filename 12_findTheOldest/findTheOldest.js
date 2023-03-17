a242e33b6da7f0210aabccfd0c98cf75c403e02b
ekoka <66888386+eekoka@users.noreply.github.com>
2023-03-17 18:30:42 +0100

    js exercise 12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+   let yearOfDeath = arr.map((people) => people.yearOfDeath);
+   let yearOfBirth = arr.map((people) => people.yearOfBirth);
+   
+   //array with age
+   let age = [];
+   yearOfBirth.forEach(calculateAge);
+   function calculateAge(yob,index) {
+     let yod = yearOfDeath[index];
+     //year of death is missing then impute todays date
+     if (yod === undefined) yod = new Date().getFullYear(); 
+     age.push(yod - yob);
+   }
 
+   let index = age.indexOf(Math.max(...age));
+   return arr[index];
 };
 
 // Do not edit below this line
